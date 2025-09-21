89495d649c2edbbe8508cb6e99c327ff55d19357
Debargha Paul <150366667+BardofCode@users.noreply.github.com>
2025-09-21 15:32:22 +0530

    Update db.js
    remove uri

--- food-del/backend/config/db.js
+++ food-del/backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const  connectDB = async () =>{
 
     // await mongoose.connect('mongodb://localhost:27017/food-del').then(()=>console.log("DB Connected"));
-    await mongoose.connect('mongodb+srv://manvithamanni13:231336@cluster0.zsw3k.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('/food-del').then(()=>console.log("DB Connected"));
    
 }
 
